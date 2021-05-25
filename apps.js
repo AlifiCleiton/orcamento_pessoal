314f5fb7797315abe0ac5081ae9781525b3f8afb
Alifi Cleiton <alificleiton123@gmail.com>
2021-05-25 16:57:36 -0300

    Feito até aula 30

--- apps.js
+++ apps.js
@@ -7,6 +7,15 @@ class Despesa{
     this.descricao = descricao
     this.valor = valor
   }
+
+  validarDados(){
+    for(let i in this){
+      if(this[i] == undefined || this[i] == '' || this[i] == null){
+        return false
+      }
+    }
+    return true
+  }
 }
 
 class Bd{
@@ -49,6 +58,29 @@ function cadastrarDespesa(){
     valor.value
   )
 
-  bd.gravar(despesa)
+  if(despesa.validarDados()){
+    bd.gravar(despesa)
+
+    document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+    document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+    document.getElementById('modal_conteudo').innerHTML = 'Despesa cadastrada com sucesso'
+    document.getElementById('modal_btn').innerHTML = 'Voltar'
+    document.getElementById('modal_btn').className = 'btn btn-success'
+
+    $('#modalRegistraDespesa').modal('show')
+    
+
+  }else{
+
+    document.getElementById('modal_titulo').innerHTML = 'Erro na inserção do registro'
+    document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+    document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente'
+    document.getElementById('modal_btn').innerHTML = 'Voltar e Corrigir'
+    document.getElementById('modal_btn').className = 'btn btn-danger'
+
+    $('#modalRegistraDespesa').modal('show')
+
+  }
+  
 }
 
